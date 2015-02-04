96f57d2a520c958947c1d1a64b3731c366d10137
Ivo Nascimento <iannsp@gmail.com>
2015-02-04 12:23:39 -0200

    add Render

--- src/Iannsp/PhpWar/Game.php
+++ src/Iannsp/PhpWar/Game.php
@@ -16,7 +16,7 @@ class Game implements \IteratorAggregate, \SplObserver
         $this->arena = $arena;
         $this->warriors = $warriors;
 
-        $this->turnControl = new Turn;
+        $this->turnControl = new Turn($this->arena->getLimits()->getX() * $this->arena->getLimits()->getY());
         $this->turnControl->attach($this);
     }
 
@@ -32,5 +32,23 @@ class Game implements \IteratorAggregate, \SplObserver
             $move = $warrior->play();
             $warrior->feedback($this->arena->play($wIdx, $move));
         }
+    }
+
+    public function render($frameRate)
+    {
+        $w = $this->arena->getLimits()->getX();
+        $h = $this->arena->getLimits()->getY();
+        $arenaArray = $this->arena->getArena();
+        $print = chr(27) . "[2J" . chr(27) . "[;H";
+        for ($x = 0; $x<$w; $x++)
+        {
+            for ($y=0; $y < $h; $y++)
+            {
+                $print.=" ".($arenaArray[$x][$y])." ";
+            }
+            $print.= "\n";
+        }
+        usleep(1000000/$frameRate);
+        return $print;
     }
 }
