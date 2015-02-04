7bd256dfe6c03a73ad7527e6a111afce76b1b174
Ivo Nascimento <iannsp@gmail.com>
2015-02-04 12:24:10 -0200

    fix algorithm
    use feedback to trace neutralizator moves.
    fix property names

--- src/Iannsp/PhpWar/Player/P1.php
+++ src/Iannsp/PhpWar/Player/P1.php
@@ -1,15 +1,16 @@
 <?php
-
 namespace Iannsp\PhpWar\Player;
 
 use Iannsp\PhpWar\Move;
 use Iannsp\PhpWar\Geometry\Cartesian;
+use Iannsp\PhpWar\Game\Feedback;
 
 class P1 implements playerInterface
 {
     private $arenaWidth  = 0;
     private $arenaHeight = 0;
-
+    private $nextMove;
+    private $lastMove;
     public function __construct(Cartesian\Point $arenaLimits)
     {
         $this->arenaWidth  = $arenaLimits->getX();
@@ -18,15 +19,22 @@ class P1 implements playerInterface
 
     public function play()
     {
+        $nextMove = $this->nextMove;
+        if(!is_null($nextMove)){
+            $this->nextMove = null;
+            return $nextMove;
+        }
         $arenaHeight = $this->arenaHeight-1;
         $arenaWidth  = $this->arenaWidth-1;
-
-        return new Move(rand(0, $arenaHeight), rand(0,$arenaWidth));
+        $this->lastMove = new Move(rand(0, $arenaHeight), rand(0,$arenaWidth));
+        return $this->lastMove;
     }
 
     public function feedback($status)
     {
-        // use it to stat you play strategy.
+        $neutralizeds = $status->getNeutralizeds();
+        if (count($neutralizeds) && $neutralizeds[0]==$this->lastMove)
+            $this->nextMove = $this->lastMove;
     }
 }
 
