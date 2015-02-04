b796f13662a955055138057d113509501e46c93b
Ivo Nascimento <iannsp@gmail.com>
2015-02-04 12:22:26 -0200

    move render to Game class. add frame rate

--- playgame.php
+++ playgame.php
@@ -9,30 +9,14 @@ $arenaLimits = new Iannsp\PhpWar\Geometry\Cartesian\Point(10,10);
 $arena = new Iannsp\PhpWar\Arena($arenaLimits, $scoreStrategy);
 $players = array (
 new Iannsp\PhpWar\Player\P1($arenaLimits),
-new Iannsp\PhpWar\Player\P2($arenaLimits)
+new Iannsp\PhpWar\Player\P1($arenaLimits)
 );
 $game = new Iannsp\PhpWar\Game($arena, $players);
 
-function render($game, $arena, $players)
-{
-   $w = $arena->getLimits()->getX();
-   $h = $arena->getLimits()->getY();
-   $arenaArray = $arena->getArena();
-print chr(27) . "[2J" . chr(27) . "[;H";
-   for ($x = 0; $x<$w; $x++)
-   {
-        for ($y=0; $y < $h; $y++)
-        {
-            echo " ".($arenaArray[$x][$y])." ";
-        }
-        echo "\n";
-   }
-}
+$frameRate = 4;
 
-
-echo "runing a game\n";
 foreach ($game as $currentTurn) {
-    render($game, $arena, $players);
+    print $game->render($frameRate);
 }
 
 echo "
